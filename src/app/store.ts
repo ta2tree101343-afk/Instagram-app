ca0b9cffb02f12fe5d301eea758887c55f8f50a8
ta2tree <ta2tree101343@gmail.com>
2026-06-07 18:37:50 +0900

    feat: configure Redux store

---
+++ src/app/store.ts
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import feedReducer from "@/features/feed/feedSlice";
+import storiesReducer from "@/features/stories/storiesSlice";
+import messagesReducer from "@/features/messages/messagesSlice";
+import profileReducer from "@/features/profile/profileSlice";
+
+export const store = configureStore({
+  reducer: {
+    feed: feedReducer,
+    stories: storiesReducer,
+    messages: messagesReducer,
+    profile: profileReducer,
+  },
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
